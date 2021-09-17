6950404f2a71f4bdff147cba79233b7cbc44f1f0
burak <burak.veziran@gmail.com>
2021-09-17 13:38:20 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     sortableMenu()
     notSortable()
     updateItemLeft()
+    updateLocalStorage()
 });
 
 var fixed = '.notSortable';
@@ -44,16 +45,18 @@ $( "#addButton" ).click(function() {
     var todoInput = $("#todo").val();
     todoList = '<li class="ui-sortable-handle">' +
                         '<input className="checkbox" type="checkbox" name="checkbox">' +
-                        '<label>' + todoInput + '</label>' +
+                        '<label name="checkboxLabel" for="checkbox">' + todoInput + '</label>' +
                '</li>';
     if (todoInput.length >= 3) {
         table.prepend(todoList);
         $("#notSortable").show("blind");
         itemLeft()
         $("#all").css(bold);
+        document.getElementById('todo').value = '';
     } else {
         alert("Todo must be at least 3 letter.")
     }
+    saveLocalStorage()
 });
 
 function itemLeft() {
@@ -79,6 +82,7 @@ function active(){
     $("#complated").css(normal);
     $("#all").css(normal);
     for (var i=0; i<inputElems.length; i++) {
+
         if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
             inputElems[i].parentNode.style.display = "none" ;
         }else if (inputElems[i].type === "checkbox" && inputElems[i].checked === false) {
@@ -144,10 +148,40 @@ function clearComplated(){
 $( "#clearComplated" ).click(function() {
     clearComplated()
     itemLeft()
+    if(inputElems.length === 1){
+        $("#notSortable").hide()
+    }
 })
 
+function saveLocalStorage() {
+    // var labels = document.querySelectorAll("label");
+    // for (var i = 0; i <= inputElems.length; i++) {
+    //     for (var j = 1; j <= (labels.length); j++) {
+    //         if (inputElems[i].type === "checkbox" && inputElems[i].checked === false) {
+    //             console.log(labels[j].innerHTML + '' + inputElems[i].checked)
+    //         } else if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
+    //             console.log(labels[j].innerHTML + '' + inputElems[i].checked)
+    //          }
+    //         }
+    //     }
+    // }
+    // var labels = document.querySelectorAll("label")
+    //
+    // for (var i = 0; i <= inputElems.length; i++) {
+    //     if (inputElems[i].type === "checkbox" && inputElems[i].checked === false) {
+    //         for (var j = 1; j <= (labels.length); j++) {
+    //             console.log(labels[j] + '' + inputElems[i].checked)
+    //         }
+    //     } else if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
+    //         for (var k = 1; k <= (labels.length); k++) {
+    //             console.log(labels[k] + '' + inputElems[i].checked)
+    //         }
+    //     }
+    // }
+}
 
-
-
+function updateLocalStorage() {
+    window.addEventListener("change", saveLocalStorage)
+}
 
 
